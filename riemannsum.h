ba450d6fa41bd22086153445e5b0251c7a1c400b
Onur Onarıcı <onuronarici@gmail.com>
2019-08-27 00:34:00 +0300

    change integrate functions argument names

--- riemannsum.h
+++ riemannsum.h
@@ -2,7 +2,7 @@
 #define RIEMANNSUM_H
 #include <vector>
 
-double integrate(std::vector<double> x, std::vector<double> y, double bound1, double bound2);
+double integrate(std::vector<double> x, std::vector<double> y, double from, double to);
 unsigned long indexOf(std::vector<double> x, double element);
 
 #endif // RIEMANNSUM_H
